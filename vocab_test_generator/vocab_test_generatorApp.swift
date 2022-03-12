9ef22f9cc160b97ccdd5f9e8c947a469f7515d0d
Jeongwon Kim <gardenk11181@snu.ac.kr>
2022-03-12 17:13:32 +0900

    Initial Commit

---
+++ vocab_test_generator/vocab_test_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  vocab_test_generatorApp.swift
+//  vocab_test_generator
+//
+//  Created by 김정원 on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct vocab_test_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
